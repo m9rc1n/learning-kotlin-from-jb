0d3b2dfa36610c7b564e9e861c5e582356bc677b
marc-inn <0.660162@gmail.com>
2015-11-08 20:26:50 +0100

    Finish task i/7 about nullable types

--- src/i_introduction/_7_Nullable_Types/NullableTypes.kt
+++ src/i_introduction/_7_Nullable_Types/NullableTypes.kt
@@ -1,25 +1,29 @@
 package i_introduction._7_Nullable_Types
 
-import util.*
+import util.TODO
+import util.doc5
 
 fun todoTask5(client: Client?, message: String?, mailer: Mailer): Nothing = TODO(
-    """
+        """
         Task 5.
         Rewrite JavaCode5.sendMessageToClient in Kotlin, using only one 'if' expression.
         Declarations of Client, PersonalInfo and Mailer are given below.
     """,
-    documentation = doc5(),
-    references = { JavaCode5().sendMessageToClient(client, message, mailer) }
+        documentation = doc5(),
+        references = { JavaCode5().sendMessageToClient(client, message, mailer) }
 )
 
 fun sendMessageToClient(
         client: Client?, message: String?, mailer: Mailer
 ) {
-    todoTask5(client, message, mailer)
+    val email = client?.personalInfo?.email
+    if (email != null && message != null) {
+        mailer.sendMessage(email, message)
+    }
 }
 
-class Client (val personalInfo: PersonalInfo?)
-class PersonalInfo (val email: String?)
+class Client(val personalInfo: PersonalInfo?)
+class PersonalInfo(val email: String?)
 
 interface Mailer {
     fun sendMessage(email: String, message: String)
