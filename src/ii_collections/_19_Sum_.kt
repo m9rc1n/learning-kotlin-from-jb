b1d59b1ec8e4b2c4987682fccbcfe9c17f94679a
marc-inn <0.660162@gmail.com>
2015-11-09 18:38:57 +0100

    Finish task ii/19 about sum

--- src/ii_collections/_19_Sum_.kt
+++ src/ii_collections/_19_Sum_.kt
@@ -4,8 +4,8 @@ fun example6() {
     val sum = listOf(1, 5, 3).sum()
 }
 
+// Return the sum of prices of all products that a customer has ordered.
+// Note: a customer may order the same product for several times.
 fun Customer.getTotalOrderPrice(): Double {
-    // Return the sum of prices of all products that a customer has ordered.
-    // Note: a customer may order the same product for several times.
-    todoCollectionTask()
+    return this.orderedProducts.sumByDouble { it.price }
 }
