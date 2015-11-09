91956b86a1028ef008a1302f2c4df3c82bec0f44
marc-inn <0.660162@gmail.com>
2015-11-09 18:08:55 +0100

    Finish task ii/14 about filter and map method

--- src/ii_collections/_14_FilterMap.kt
+++ src/ii_collections/_14_FilterMap.kt
@@ -8,14 +8,9 @@ fun example1(list: List<Int>) {
     val squares = list.map { it * it }
 }
 
-fun Shop.getCitiesCustomersAreFrom(): Set<City> {
-    // Return the set of cities the customers are from
-    todoCollectionTask()
-}
+// Return the set of cities the customers are from
+fun Shop.getCitiesCustomersAreFrom() = this.customers.map { it.city }.toSet()
 
-fun Shop.getCustomersFrom(city: City): List<Customer> {
-    // Return a list of the customers who live in the given city
-    todoCollectionTask()
-}
-
+// Return a list of the customers who live in the given city
+fun Shop.getCustomersFrom(city: City) = this.customers.filter { it.city.equals(city) }.toList()
 
